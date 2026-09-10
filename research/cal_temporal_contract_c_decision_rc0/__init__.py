75242634cc6c638f961035d2f2dc3f8570308090
Cameron Sanderson <camerontjs@gmail.com>
2026-09-09 23:39:57 -0400

    research: freeze temporal Contract C projection package

---
+++ research/cal_temporal_contract_c_decision_rc0/__init__.py
@@ -0,0 +1,5 @@
+"""Research-only temporal Contract C / Decision RC0 conformance package."""
+
+from .projection import ProjectionRefusal, ProjectionResult, project_temporal_contract_c
+
+__all__ = ["ProjectionRefusal", "ProjectionResult", "project_temporal_contract_c"]
